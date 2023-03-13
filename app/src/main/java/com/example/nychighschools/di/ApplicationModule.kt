354569bbceb325477b4bed11f14f1fc62fbfd76a
riddhi ch <chriddhireddy@gmail.com>
2023-03-13 01:15:35 -0500

    Modified README document

--- app/src/main/java/com/example/nychighschools/di/ApplicationModule.kt
+++ app/src/main/java/com/example/nychighschools/di/ApplicationModule.kt
@@ -16,26 +16,6 @@ class ApplicationModule(private val application: Application) {
     fun provideContext(): Context = application.applicationContext
 
     /**
-     * Method that provides Database
-     *//*
-    @Provides
-    fun providesSchoolsDatabase(context: Context): SchoolsDatabase =
-        Room.databaseBuilder(
-            context,
-            SchoolsDatabase::class.java,
-            DB_NAME
-        ).build()
-
-    *//**
-     * Method that provides DAO
-     *//*
-    @Provides
-    fun providesSchoolDAO(
-        schoolsDatabase: SchoolsDatabase
-    ): SchoolsDAO =
-        schoolsDatabase.getSchoolsDAO()
-
-    *//**
      * Methods that provides View Model Factory
      */
     @Provides
@@ -43,9 +23,6 @@ class ApplicationModule(private val application: Application) {
         repository: SchoolsRepository,
         ioDispatcher: CoroutineDispatcher
     ): SchoolsViewModelFactory =
-        SchoolsViewModelFactory(repository,ioDispatcher)
+        SchoolsViewModelFactory(repository, ioDispatcher)
 
-    private companion object{
-        const val DB_NAME = "nycSchools-db"
-    }
 }
